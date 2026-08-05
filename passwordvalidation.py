4d61227e62e9131f238f983d51f706460eb492fa
prabhu <prabhuls2005@gmail.com>
2026-08-05 10:44:24 +0530

    Implement password validation GUI with Tkinter

---
+++ passwordvalidation.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+
+root.geometry("650x500")
+root.title("Password Validation")
+
+password = tk.StringVar()
+
+passentry = ttk.Entry(root, textvariable=password, justify="center", show="*")
+passentry.pack(pady=10)
+
+resultlbl = tk.StringVar()
+
+
+def validation(userpass):
+    errors = []
+
+    if len(userpass) < 8:
+        errors.append("Password should contain at least 8 characters.")
+
+    if not any(c.isupper() for c in userpass):
+        errors.append("Password must contain at least one uppercase letter.")
+
+    if not any(c.islower() for c in userpass):
+        errors.append("Password must contain at least one lowercase letter.")
+
+    if not any(c.isdigit() for c in userpass):
+        errors.append("Password must contain at least one digit.")
+
+    if not any(not c.isalnum() for c in userpass):
+        errors.append("Password must contain at least one special character.")
+
+    if errors:
+        resultlbl.set("\n".join(errors))
+    else:
+        resultlbl.set("Your password was saved successfully!")
+        password.set("")
+
+
+ttk.Button(root, text="Submit",
+           command=lambda: validation(password.get())).pack(pady=10)
+
+ttk.Label(root, textvariable=resultlbl, foreground="red").pack()
+
+root.mainloop()
