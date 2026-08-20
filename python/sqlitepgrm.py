a743261bce3c59801c7edff1e140596726f3a267
prabhu <prabhuls2005@gmail.com>
2026-08-20 04:48:25 +0000

    sqlite py pgrm

--- python/sqlitepgrm.py
+++ python/sqlitepgrm.py
@@ -2,12 +2,34 @@ import sqlite3
 
 conn = sqlite3.connect("student.db")
 
+
+def insertData(data,cursor,connection):
+    inscmd ='''INSERT INTO student (name,age) VALUES (?,?)'''
+    cursor.execute(inscmd,data)
+    connection.commit()
+
 cur = conn.cursor()
 
 cmd1 = ''' CREATE TABLE IF NOT EXISTS student(
-    name TEXT
-)'''
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR,
+    age NUMBER
+ )'''
 cur.execute(cmd1)
 conn.commit()
+
+
+
+# Insert a new student
+insertData(("prabhu",21),cur,conn)
+insertData(("arun",21),cur,conn)
+insertData(("ram",21),cur,conn)
+
+
+cur.execute("SELECT * FROM student")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
 conn.close()
 
